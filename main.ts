2e08394a9e401495209de00657002b9f621bec62
pclemon <38698371+pclemon@users.noreply.github.com>
2020-07-13 21:28:00 -0400

    Officially added working fx8

--- main.ts
+++ main.ts
@@ -8,16 +8,18 @@ class Vector {
         this.y = y;
     }
 
-    public xCoordinate(): number{
-        return Fx.toInt(this.x);
+    public xCoordinate(): Fx8{
+        //return Fx.toInt(this.x);
+        return this.x;
     }
     
-    public yCoordinate(): number{
-        return Fx.toInt(this.y);
+    public yCoordinate(): Fx8{
+        //return Fx.toInt(this.y);
+        return this.y;
     }
 
     public print(){
-        console.log(this.x + ", " + this.y);
+        console.log(Fx.toFloat(this.x) + ", " + Fx.toFloat(this.y));
     }
 }
 
@@ -30,47 +32,50 @@ namespace Vector{
     //% weight=100
     export function createVector(x: number, y: number){
         return new Vector(Fx8(x), Fx8(y));
+        //return new Vector(x, y);
     }
 
     //% block="vector one $vector1 plus vector two $vector2"
     export function vectorAdd(vector1: Vector, vector2: Vector): Vector{
-        return new Vector(Fx8(vector1.xCoordinate() + vector2.xCoordinate()), Fx8(vector1.yCoordinate() + vector2.yCoordinate()));
+        return new Vector(Fx.add(vector1.xCoordinate(),vector2.xCoordinate()), Fx.add(vector1.yCoordinate(), vector2.yCoordinate()));
     }
 
     //% block="vector one $vector1 minus vector two $vector2"
     export function vectorSub(vector1: Vector, vector2: Vector): Vector{
-        return new Vector(Fx8(vector1.xCoordinate() - vector2.xCoordinate()), Fx8(vector1.yCoordinate() - vector2.yCoordinate()));
+        return new Vector(Fx.sub(vector1.xCoordinate(), vector2.xCoordinate()), Fx.sub(vector1.yCoordinate(), vector2.yCoordinate()));
     }
 
     //% block="vector one $vector1 times vector two $vector2"
     export function vectorMultiplication(vector1: Vector, vector2: Vector): Vector{
-        return new Vector(Fx8(vector1.xCoordinate() * vector2.xCoordinate()), Fx8(vector1.yCoordinate() * vector2.yCoordinate()));
+        return new Vector(Fx.mul(vector1.xCoordinate(), vector2.xCoordinate()), Fx.mul(vector1.yCoordinate(), vector2.yCoordinate()));
     }
+
     //% block="magnitude of vector $vector"
     export function vectorMagnitude(vector: Vector): number{
-        return Math.sqrt((vector.xCoordinate() * vector.xCoordinate()) + (vector.yCoordinate() * vector.yCoordinate()));
+        return Math.sqrt(Fx.toFloat(Fx.add(Fx.mul(vector.xCoordinate(), vector.xCoordinate()), Fx.mul(vector.yCoordinate(), vector.yCoordinate()))));
     }
 
     //% block="scale vector $vector by $num"
     export function vectorScale(vector: Vector, num: number): Vector{
-        return new Vector(Fx8(vector.xCoordinate() * num), Fx8(vector.yCoordinate() * num));
+        return new Vector(Fx.mul(vector.xCoordinate(), Fx8(num)), Fx.mul(vector.yCoordinate(), Fx8(num)));
     }
 
     //% block="dot product of vector one $vector1 and vector two $vector2"
     export function dotProduct(vector1: Vector, vector2: Vector): number{
         let mulVec = vectorMultiplication(vector1, vector2);
-        return (mulVec.xCoordinate() + mulVec.yCoordinate());
+        return (Fx.toFloat((Fx.add(mulVec.xCoordinate(), mulVec.yCoordinate()))));
     }
 
     //% block="distance between vector one $vector1 and vector two $vector2"
     export function euclideanDistance(vector1: Vector, vector2: Vector): number{
         let tempVec = vectorSub(vector1, vector2);
-        return ((tempVec.xCoordinate() * tempVec.xCoordinate()) + (tempVec.yCoordinate() * tempVec.yCoordinate()));
+        return Math.sqrt((Fx.toFloat(Fx.add(Fx.mul(tempVec.xCoordinate(), tempVec.xCoordinate()), Fx.mul(tempVec.yCoordinate(), tempVec.yCoordinate())))));
     }
 
     //Print the vector coordinate's via the console
     //% block="print vector $vectorPrint"
     export function printVector(vectorPrint: Vector){
+        //console.log(vectorPrint.xCoordinate() + ", " + vectorPrint.yCoordinate());
         vectorPrint.print();
     }
 
